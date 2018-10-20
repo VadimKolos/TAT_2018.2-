8ac90ceb84e619ece88e6a55374cba08a351d07d
User <User@WIN-R1VEPF5FUNI>
2018-10-20 18:49:40 +0300

    initial commit

---
+++ myfstprojectt/MaxUniqueSequnceFinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfstprojectt
+{
+    class MaxUniqueSequnceFinder
+    {
+        char[] massiv;
+        public void enter(string[] args) //enter the string
+        {
+            Console.Write("enter the string: ");
+           
+            string str = null;
+            if (args.Length != 0)
+                foreach (string s in args)
+                {
+                    str = s;
+                    massiv = str.ToCharArray();
+                }
+             //
+           Console.Write(massiv);
+            Console.ReadLine();
+        }
+
+        public void check() //counting different values
+        {
+            int allsymbols = 0;
+            int double_symbol = 0;
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                allsymbols++; // string length
+                for (int j = i + 1; j < massiv.Length; j++)
+                {
+                    if (massiv[j] == massiv[i])
+                    {
+                        double_symbol++; //repeated characters
+                        break;
+                    }
+                }
+            }
+            int unicSymbols = allsymbols - double_symbol;
+            Console.Write("count of unique symbols - " + unicSymbols);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ myfstprojectt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfstprojectt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxUniqueSequnceFinder name = new MaxUniqueSequnceFinder();
+            name.enter(args);
+            name.check();
+        }
+    }
+}
